28252824a05afe372da58258116a6a167edcb96d
Dinay Singh <dinay@gmail.com>
2024-10-30 02:29:21 +0530

    Learn about routing

--- day8_ReactRouter/src/App.jsx
+++ day8_ReactRouter/src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import Header from './Components/Header/Header.jsx'
+import Home from './Components/Home/Home'
+import Footer from './Components/Footer/Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='bg-gray-500 text-3xl'>React Router</h1>
+
     </>
   )
 }
--- day8_ReactRouter/src/main.jsx
+++ day8_ReactRouter/src/main.jsx
@@ -1,10 +1,45 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import Router from './Router.jsx'
+import Home from './Components/Home/Home.jsx'
+import About from './Components/About/About.jsx'
+import Contact from './Components/ContactUs/Contact.jsx'
+
+// const router = createBrowserRouter([
+//   {
+//     path:"/",
+//     element: <Router/>,
+//     children:[
+//       {
+//         path:"/",
+//         element:<Home/>
+//       },
+//       {
+//         path:"/about",
+//         element:<About/>
+//       },
+//       {
+//         path:"/contact",
+//         element:<Contact/>
+//       }
+//     ]
+//   }
+// ])
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Router />} >
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+    </Route>
+
+  ))
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
